395a7c5d518dbabaf892bb27a9fbcb1e05cfb735
PawSpaceIND <karthik@pawspace.in>
2026-08-08 19:15:03 +0530

    Fix Control Center sidebar containment

---
+++ app/control/layout.tsx
@@ -0,0 +1,5 @@
+import "./control-route-fix.css";
+
+export default function ControlLayout({children}:{children:React.ReactNode}){
+  return <div className="control-route-shell">{children}</div>;
+}
