cd62be2f450083e73526815968578a20fc412ca8
PawSpaceIND <karthik@pawspace.in>
2026-09-18 10:52:22 +0530

    feat(v2): unify partner CRM and control center entry points

---
+++ app/v2/partner/page.tsx
@@ -0,0 +1,3 @@
+import PartnerMobileLayout from "../../partner-app/layout";
+import PartnerPage from "../../partner-app/page";
+export default function V2Partner(){return <PartnerMobileLayout><PartnerPage/></PartnerMobileLayout>;}
