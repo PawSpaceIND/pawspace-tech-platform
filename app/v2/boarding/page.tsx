b87fc39cc9050ae2845562126f3b8dba4309cf73
PawSpaceIND <karthik@pawspace.in>
2026-09-18 10:07:07 +0530

    feat(v2): add native boarding and sitting shell

---
+++ app/v2/boarding/page.tsx
@@ -0,0 +1,2 @@
+import V2StayExperience from "../stay-experience";
+export default function V2Boarding(){return <V2StayExperience mode="boarding"/>;}
---
+++ app/v2/sitting/page.tsx
@@ -0,0 +1,2 @@
+import V2StayExperience from "../stay-experience";
+export default function V2Sitting(){return <V2StayExperience mode="sitting"/>;}
