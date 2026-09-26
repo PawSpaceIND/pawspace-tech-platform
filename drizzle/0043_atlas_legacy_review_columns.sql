c4614f4b0a9183d5caf9e7590bcf21e397236612
PawSpaceIND <karthik@pawspace.in>
2026-09-26 16:38:36 +0530

    Verify migrated Atlas audit columns in remote SQL runner

--- drizzle/0043_atlas_legacy_review_columns.sql
+++ drizzle/0043_atlas_legacy_review_columns.sql
@@ -8,3 +8,6 @@
 -- @add-column-if-missing atlas_proposal_reviews|review_sequence|INTEGER
 -- @add-column-if-missing atlas_proposal_reviews|previous_review_hash|TEXT
 -- @add-column-if-missing atlas_proposal_reviews|review_hash|TEXT
+
+-- Verify the deployed shape after the guarded additions.
+SELECT reviewer_role_code,reviewer_authority_source,review_auth_assurance_type,review_auth_session_id,review_mfa_verified_at,review_hash_version,review_sequence,previous_review_hash,review_hash FROM atlas_proposal_reviews LIMIT 0;
